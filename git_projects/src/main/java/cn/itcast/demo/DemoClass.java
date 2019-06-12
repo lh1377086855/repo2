045e2d14d575de309f5d3ee162663c4170f24800
luohao <1377086855@qq.com>
2019-06-12 18:18:07 +0800

    把本地工程添加到git

---
+++ git_projects/src/main/java/cn/itcast/demo/DemoClass.java
@@ -0,0 +1,18 @@
+package cn.itcast.demo;
+
+public class DemoClass {
+
+    public static void main(String[] args) {
+        System.out.println("1234567");
+        System.out.println("1234567");
+        System.out.println("1234567");
+        System.out.println("1234567");
+        System.out.println("1234567");
+        System.out.println("1234567");
+        System.out.println("1234567");
+        System.out.println("1234567");
+        System.out.println("1234567");
+
+    }
+
+}
